7f87d389d8242adf0ea79ea7a64345c3335c2b78
Aris <165860478+aris012@users.noreply.github.com>
2024-12-05 14:48:40 +1300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../src/components/layout/Layout";
+import HomePage from "./pages/home/HomePage";
+import AboutPage from "./pages/about/AboutPage";
+import PaintingPage from "./pages/painting/PaintingPage";
+import GraphicsPage from "./pages/graphics/GraphicsPage";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="about" element={<AboutPage />} />
+            <Route path="graphics" element={<GraphicsPage />} />
+            <Route path="painting" element={<PaintingPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
